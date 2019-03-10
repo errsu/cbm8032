f0d60d99d1e475db431cba21c51a3d1cada440dc
errsu <errsuberlin@gmail.com>
2019-03-10 14:34:53 +0100

    remove vector graphics support from libshapes

--- openvgerrsu/libshapes.c
+++ openvgerrsu/libshapes.c
@@ -205,8 +205,8 @@ void Scale(VGfloat x, VGfloat y) {
 // Style functions
 //
 
-// SetFill sets the fill color
-void SetFill(VGfloat color[4]) {
+// setFill sets the fill color
+static void setFill(VGfloat color[4]) {
 	VGPaint fillPaint = vgCreatePaint();
 	vgSetParameteri(fillPaint, VG_PAINT_TYPE, VG_PAINT_TYPE_COLOR);
 	vgSetParameterfv(fillPaint, VG_PAINT_COLOR, 4, color);
@@ -214,8 +214,8 @@ void SetFill(VGfloat color[4]) {
 	vgDestroyPaint(fillPaint);
 }
 
-// SetStroke sets the stroke color
-void SetStroke(VGfloat color[4]) {
+// setStroke sets the stroke color
+static void setStroke(VGfloat color[4]) {
 	VGPaint strokePaint = vgCreatePaint();
 	vgSetParameteri(strokePaint, VG_PAINT_TYPE, VG_PAINT_TYPE_COLOR);
 	vgSetParameterfv(strokePaint, VG_PAINT_COLOR, 4, color);
@@ -223,8 +223,8 @@ void SetStroke(VGfloat color[4]) {
 	vgDestroyPaint(strokePaint);
 }
 
-// StrokeWidth sets the stroke width
-void StrokeWidth(VGfloat width) {
+// strokeWidth sets the stroke width
+static void strokeWidth(VGfloat width) {
 	vgSetf(VG_STROKE_LINE_WIDTH, width);
 	vgSeti(VG_STROKE_CAP_STYLE, VG_CAP_BUTT);
 	vgSeti(VG_STROKE_JOIN_STYLE, VG_JOIN_MITER);
@@ -260,48 +260,11 @@ void RGB(unsigned int r, unsigned int g, unsigned int b, VGfloat color[4]) {
 	RGBA(r, g, b, 1.0f, color);
 }
 
-// Stroke sets the stroke color, defined as a RGB triple.
-void Stroke(unsigned int r, unsigned int g, unsigned int b, VGfloat a) {
-	VGfloat color[4];
-	RGBA(r, g, b, a, color);
-	SetStroke(color);
-}
-
 // Fill sets the fillcolor, defined as a RGBA quad.
 void Fill(unsigned int r, unsigned int g, unsigned int b, VGfloat a) {
 	VGfloat color[4];
 	RGBA(r, g, b, a, color);
-	SetFill(color);
-}
-
-// setstops sets color stops for gradients
-void setstop(VGPaint paint, VGfloat * stops, int n) {
-	VGboolean multmode = VG_FALSE;
-	VGColorRampSpreadMode spreadmode = VG_COLOR_RAMP_SPREAD_REPEAT;
-	vgSetParameteri(paint, VG_PAINT_COLOR_RAMP_SPREAD_MODE, spreadmode);
-	vgSetParameteri(paint, VG_PAINT_COLOR_RAMP_PREMULTIPLIED, multmode);
-	vgSetParameterfv(paint, VG_PAINT_COLOR_RAMP_STOPS, 5 * n, stops);
-	vgSetPaint(paint, VG_FILL_PATH);
-}
-
-// LinearGradient fills with a linear gradient
-void FillLinearGradient(VGfloat x1, VGfloat y1, VGfloat x2, VGfloat y2, VGfloat * stops, int ns) {
-	VGfloat lgcoord[4] = { x1, y1, x2, y2 };
-	VGPaint paint = vgCreatePaint();
-	vgSetParameteri(paint, VG_PAINT_TYPE, VG_PAINT_TYPE_LINEAR_GRADIENT);
-	vgSetParameterfv(paint, VG_PAINT_LINEAR_GRADIENT, 4, lgcoord);
-	setstop(paint, stops, ns);
-	vgDestroyPaint(paint);
-}
-
-// RadialGradient fills with a linear gradient
-void FillRadialGradient(VGfloat cx, VGfloat cy, VGfloat fx, VGfloat fy, VGfloat radius, VGfloat * stops, int ns) {
-	VGfloat radialcoord[5] = { cx, cy, fx, fy, radius };
-	VGPaint paint = vgCreatePaint();
-	vgSetParameteri(paint, VG_PAINT_TYPE, VG_PAINT_TYPE_RADIAL_GRADIENT);
-	vgSetParameterfv(paint, VG_PAINT_RADIAL_GRADIENT, 5, radialcoord);
-	setstop(paint, stops, ns);
-	vgDestroyPaint(paint);
+	setFill(color);
 }
 
 // ClipRect limits the drawing area to specified rectangle
@@ -316,117 +279,15 @@ void ClipEnd() {
 	vgSeti(VG_SCISSORING, VG_FALSE);
 }
 
-// newpath creates path data
-// Changed capabilities as others not needed at the moment - allows possible
-// driver optimisations.
-VGPath newpath() {
-	return vgCreatePath(VG_PATH_FORMAT_STANDARD, VG_PATH_DATATYPE_F, 1.0f, 0.0f, 0, 0, VG_PATH_CAPABILITY_APPEND_TO);	// Other capabilities not needed
-}
-
-// makecurve makes path data using specified segments and coordinates
-void makecurve(VGubyte * segments, VGfloat * coords, VGbitfield flags) {
-	VGPath path = newpath();
-	vgAppendPathData(path, 2, segments, coords);
-	vgDrawPath(path, flags);
-	vgDestroyPath(path);
-}
-
-// CBezier makes a quadratic bezier curve
-void Cbezier(VGfloat sx, VGfloat sy, VGfloat cx, VGfloat cy, VGfloat px, VGfloat py, VGfloat ex, VGfloat ey) {
-	VGubyte segments[] = { VG_MOVE_TO_ABS, VG_CUBIC_TO };
-	VGfloat coords[] = { sx, sy, cx, cy, px, py, ex, ey };
-	makecurve(segments, coords, VG_FILL_PATH | VG_STROKE_PATH);
-}
-
-// QBezier makes a quadratic bezier curve
-void Qbezier(VGfloat sx, VGfloat sy, VGfloat cx, VGfloat cy, VGfloat ex, VGfloat ey) {
-	VGubyte segments[] = { VG_MOVE_TO_ABS, VG_QUAD_TO };
-	VGfloat coords[] = { sx, sy, cx, cy, ex, ey };
-	makecurve(segments, coords, VG_FILL_PATH | VG_STROKE_PATH);
-}
-
-// interleave interleaves arrays of x, y into a single array
-void interleave(VGfloat * x, VGfloat * y, int n, VGfloat * points) {
-	while (n--) {
-		*points++ = *x++;
-		*points++ = *y++;
-	}
-}
-
-// poly makes either a polygon or polyline
-void poly(VGfloat * x, VGfloat * y, VGint n, VGbitfield flag) {
-	VGfloat points[n * 2];
-	VGPath path = newpath();
-	interleave(x, y, n, points);
-	vguPolygon(path, points, n, VG_FALSE);
-	vgDrawPath(path, flag);
-	vgDestroyPath(path);
-}
-
-// Polygon makes a filled polygon with vertices in x, y arrays
-void Polygon(VGfloat * x, VGfloat * y, VGint n) {
-	poly(x, y, n, VG_FILL_PATH);
-}
-
-// Polyline makes a polyline with vertices at x, y arrays
-void Polyline(VGfloat * x, VGfloat * y, VGint n) {
-	poly(x, y, n, VG_STROKE_PATH);
-}
-
-// Rect makes a rectangle at the specified location and dimensions
-void Rect(VGfloat x, VGfloat y, VGfloat w, VGfloat h) {
-	VGPath path = newpath();
-	vguRect(path, x, y, w, h);
-	vgDrawPath(path, VG_FILL_PATH | VG_STROKE_PATH);
-	vgDestroyPath(path);
-}
-
-// Line makes a line from (x1,y1) to (x2,y2)
-void Line(VGfloat x1, VGfloat y1, VGfloat x2, VGfloat y2) {
-	VGPath path = newpath();
-	vguLine(path, x1, y1, x2, y2);
-	vgDrawPath(path, VG_STROKE_PATH);
-	vgDestroyPath(path);
-}
-
-// Roundrect makes an rounded rectangle at the specified location and dimensions
-void Roundrect(VGfloat x, VGfloat y, VGfloat w, VGfloat h, VGfloat rw, VGfloat rh) {
-	VGPath path = newpath();
-	vguRoundRect(path, x, y, w, h, rw, rh);
-	vgDrawPath(path, VG_FILL_PATH | VG_STROKE_PATH);
-	vgDestroyPath(path);
-}
-
-// Ellipse makes an ellipse at the specified location and dimensions
-void Ellipse(VGfloat x, VGfloat y, VGfloat w, VGfloat h) {
-	VGPath path = newpath();
-	vguEllipse(path, x, y, w, h);
-	vgDrawPath(path, VG_FILL_PATH | VG_STROKE_PATH);
-	vgDestroyPath(path);
-}
-
-// Circle makes a circle at the specified location and dimensions
-void Circle(VGfloat x, VGfloat y, VGfloat r) {
-	Ellipse(x, y, r, r);
-}
-
-// Arc makes an elliptical arc at the specified location and dimensions
-void Arc(VGfloat x, VGfloat y, VGfloat w, VGfloat h, VGfloat sa, VGfloat aext) {
-	VGPath path = newpath();
-	vguArc(path, x, y, w, h, sa, aext, VGU_ARC_OPEN);
-	vgDrawPath(path, VG_FILL_PATH | VG_STROKE_PATH);
-	vgDestroyPath(path);
-}
-
 // Start begins the picture, clearing a rectangular region with a specified color
 void Start(int width, int height) {
 	VGfloat color[4] = { 1, 1, 1, 1 };
 	vgSetfv(VG_CLEAR_COLOR, 4, color);
 	vgClear(0, 0, width, height);
 	color[0] = 0, color[1] = 0, color[2] = 0;
-	SetFill(color);
-	SetStroke(color);
-	StrokeWidth(0);
+	setFill(color);
+	setStroke(color);
+	strokeWidth(0);
 	vgLoadIdentity();
 }
 
@@ -479,60 +340,4 @@ void WindowClear() {
 // transformations)
 void AreaClear(unsigned int x, unsigned int y, unsigned int w, unsigned int h) {
 	vgClear(x, y, w, h);
-}
-
-// Outlined shapes
-// Hollow shapes -because filling still happens even with a fill of 0,0,0,0
-// unlike where using a strokewidth of 0 disables the stroke.
-// Either this or change the original functions to require the VG_x_PATH flags
-
-// CBezier makes a quadratic bezier curve, stroked
-void CbezierOutline(VGfloat sx, VGfloat sy, VGfloat cx, VGfloat cy, VGfloat px, VGfloat py, VGfloat ex, VGfloat ey) {
-	VGubyte segments[] = { VG_MOVE_TO_ABS, VG_CUBIC_TO };
-	VGfloat coords[] = { sx, sy, cx, cy, px, py, ex, ey };
-	makecurve(segments, coords, VG_STROKE_PATH);
-}
-
-// QBezierOutline makes a quadratic bezier curve, outlined
-void QbezierOutline(VGfloat sx, VGfloat sy, VGfloat cx, VGfloat cy, VGfloat ex, VGfloat ey) {
-	VGubyte segments[] = { VG_MOVE_TO_ABS, VG_QUAD_TO };
-	VGfloat coords[] = { sx, sy, cx, cy, ex, ey };
-	makecurve(segments, coords, VG_STROKE_PATH);
-}
-
-// RectOutline makes a rectangle at the specified location and dimensions, outlined
-void RectOutline(VGfloat x, VGfloat y, VGfloat w, VGfloat h) {
-	VGPath path = newpath();
-	vguRect(path, x, y, w, h);
-	vgDrawPath(path, VG_STROKE_PATH);
-	vgDestroyPath(path);
-}
-
-// RoundrectOutline  makes an rounded rectangle at the specified location and dimensions, outlined
-void RoundrectOutline(VGfloat x, VGfloat y, VGfloat w, VGfloat h, VGfloat rw, VGfloat rh) {
-	VGPath path = newpath();
-	vguRoundRect(path, x, y, w, h, rw, rh);
-	vgDrawPath(path, VG_STROKE_PATH);
-	vgDestroyPath(path);
-}
-
-// EllipseOutline makes an ellipse at the specified location and dimensions, outlined
-void EllipseOutline(VGfloat x, VGfloat y, VGfloat w, VGfloat h) {
-	VGPath path = newpath();
-	vguEllipse(path, x, y, w, h);
-	vgDrawPath(path, VG_STROKE_PATH);
-	vgDestroyPath(path);
-}
-
-// CircleOutline makes a circle at the specified location and dimensions, outlined
-void CircleOutline(VGfloat x, VGfloat y, VGfloat r) {
-	EllipseOutline(x, y, r, r);
-}
-
-// ArcOutline makes an elliptical arc at the specified location and dimensions, outlined
-void ArcOutline(VGfloat x, VGfloat y, VGfloat w, VGfloat h, VGfloat sa, VGfloat aext) {
-	VGPath path = newpath();
-	vguArc(path, x, y, w, h, sa, aext, VGU_ARC_OPEN);
-	vgDrawPath(path, VG_STROKE_PATH);
-	vgDestroyPath(path);
 }
